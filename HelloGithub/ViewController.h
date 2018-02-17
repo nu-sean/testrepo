161bddd39ac6f36f486b1af4b8852330214f0a84
mac <mac@macs-macbook-pro.local>
2018-02-17 12:11:47 +0600

    Initial Commit

---
+++ HelloGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGithub
+//
+//  Created by mac on 17/2/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloGithub/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloGithub
+//
+//  Created by mac on 17/2/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
