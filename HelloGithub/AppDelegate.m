5cfd3c549042898f17d726294963b17b8bfe9dda
mac <mac@macs-macbook-pro.local>
2018-10-15 14:05:46 +0600

    changes 1

--- HelloGithub/AppDelegate.m
+++ HelloGithub/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    //hello Development branch
+    //
     return YES;
 }
 
